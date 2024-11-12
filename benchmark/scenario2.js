f2279c1a950fee3d7c3b7930cd22e48ae8b091bf
AlixByr <alixbyr@gmail.com>
2024-11-12 17:38:27 +0100

    Modif greenframe

--- benchmark/scenario2.js
+++ benchmark/scenario2.js
@@ -1,5 +1,5 @@
 const visit = async (page) => {
-    await page.goto('2024-10-07%2006:35', {
+    await page.goto('http://localhost/search?q=veste%20en%20cuir', {
         waitUntil: 'networkidle',
     });
     await page.waitForTimeout(10000);
